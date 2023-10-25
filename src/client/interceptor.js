f05e7a1da9c91e39f18f168a3cc2b322375ecf94
Logan Thomas <136652260+stylo-codes-stuff@users.noreply.github.com>
2023-10-25 22:35:25 +0000

    organized files that have to do with the proxy client

--- src/client/interceptor.js
+++ src/client/interceptor.js
@@ -1,17 +1,19 @@
-export function handle_request(requestDetails){
-    console.log('Redirecting: ${requestDetails.url}');
-    if (requestDetails.url === targetUrl){
-        return;
-    }
-    return{
-        redirectURL: targetUrl,
+let targetPage = "http://useragentstring.com/*";
 
-    };
+let ua =
+  "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16";
+
+function rewriteUserAgentHeader(e) {
+  e.requestHeaders.forEach((header) => {
+    if (header.name.toLowerCase() === "user-agent") {
+      header.value = ua;
+    }
+  });
+  return { requestHeaders: e.requestHeaders };
 }
 
-browser.webRequest.onBeforeRequest.addListener(
-    handle_request,
-    {urls: [pattern], types: ["image"]},
-    ["blocking"],
-)
-
+browser.webRequest.onBeforeSendHeaders.addListener(
+  rewriteUserAgentHeader,
+  { urls: [targetPage] },
+  ["blocking", "requestHeaders"],
+);
