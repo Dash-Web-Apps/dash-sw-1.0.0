0216529473840b717e8d46b13cd3bf166c88f342
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2023-10-26 19:13:12 +0000

    made and test ran a simple http server and service worker

--- src/server.mjs
+++ src/server.mjs
@@ -1,18 +1,11 @@
-import http from 'node:http';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const http = require('node:http');
+import "http";
 import { createBareServer } from '@tomphttp/bare-server-node';
-import { SocksProxyAgent } from 'socks-proxy-agent';
-
-// TOR daemon listens on port 9050 by default
-const socksProxyAgent = new SocksProxyAgent(
-	`socks://127.0.0.1:${process.env.TOR_PORT || '9050'}`
-);
 
 const httpServer = http.createServer();
 
-const bareServer = createBareServer('/', {
-	httpAgent: socksProxyAgent,
-	httpsAgent: socksProxyAgent,
-});
+const bareServer = createBareServer('/');
 
 httpServer.on('request', (req, res) => {
 	if (bareServer.shouldRoute(req)) {
